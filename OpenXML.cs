698f7bddc8ec80a4a9ab55efa05bc6d36a1d1c31
cyanez <chris.yanez@gmail.com>
2020-07-06 07:01:38 -0500

    Insert a new Worksheet

--- OpenXML.cs
+++ OpenXML.cs
@@ -33,6 +33,28 @@ namespace OpenXMLManager {
 
     }
 
+    public static void InsertWorksheet(string filepath, string sheetName) {
+      
+      using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(filepath, true)) {
+        
+        WorksheetPart newWorksheetPart = spreadSheet.WorkbookPart.AddNewPart<WorksheetPart>();
+        newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+
+        Sheets sheets = spreadSheet.WorkbookPart.Workbook.GetFirstChild<Sheets>();
+        string relationshipId = spreadSheet.WorkbookPart.GetIdOfPart(newWorksheetPart);
+
+        
+        uint sheetId = 1;
+        if (sheets.Elements<Sheet>().Count() > 0) {
+          sheetId = sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+        }
+       
+        Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+        sheets.Append(sheet);
+      }
+
+    }
+
 
   }
 }
