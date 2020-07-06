cd1a522d28b0ae568d9b45935e1c4a5c2d63970d
cyanez <chris.yanez@gmail.com>
2020-07-06 08:06:31 -0500

    Update Cell Value

--- OpenXML.cs
+++ OpenXML.cs
@@ -55,6 +55,105 @@ namespace OpenXMLManager {
 
     }
 
+    public static void UpdateCell(string fileName, string sheetName, string columnName, uint rowIndex, string cellValue) {
+
+      OpenSrepadSheetDocument(fileName, sheetName, columnName, rowIndex, cellValue, new EnumValue<CellValues>(CellValues.String));
+    }
+    public static void OpenSrepadSheetDocument(string fileName, string sheetName, string columnName, uint rowIndex, string cellValue, EnumValue<CellValues> cellDataType) {
+      using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, true)) {
+        WorkbookPart workbookPart = document.WorkbookPart;
+        WorksheetPart worksheetPart = GetSheet(workbookPart, sheetName);
+
+        if (worksheetPart == null) {
+          throw new Exception("I cant find sheet: " + sheetName);
+        }
+
+        Row row = GetRow(worksheetPart.Worksheet, rowIndex);
+        Cell cell = InsertCell(worksheetPart.Worksheet, row, columnName, rowIndex);
+
+        cell.CellValue = new CellValue(cellValue);
+                   
+        cell.DataType = cellDataType;
+                    
+        worksheetPart.Worksheet.Save();
+
+
+
+
+
+
+
+
+
+
+
+        // Cell cell = GetCell(worksheetPart, addressName);
+
+        //  MessageBox.Show(GetCellValue(workbookPart,cell));
+
+      }
+
+
+
+    }
+
+    private static WorksheetPart GetSheet(WorkbookPart workbookPart, string sheetName) {      
+      Sheet sheet = workbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName).FirstOrDefault();
+            
+      if (sheet == null) {
+        throw new ArgumentException("I cant find sheet: " + sheetName);
+      }
+
+      return (WorksheetPart) (workbookPart.GetPartById(sheet.Id));
+    }
+
+    private static bool ExistRow(Worksheet worksheet, uint rowIndex) {
+      int count = worksheet.GetFirstChild<SheetData>().Elements<Row>().Where(r => r.RowIndex == rowIndex).Count();
+
+      if (count == 0) {
+        return false;
+      }
+
+      return true;      
+    }
+
+    private static Row GetRow(Worksheet worksheet, uint rowIndex) {
+      if (!ExistRow(worksheet, rowIndex)) {
+        Row row = new Row() {
+          RowIndex = rowIndex
+        };
+        SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+        sheetData.Append(row);
+      }
+
+      return worksheet.GetFirstChild<SheetData>().Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+    }
+
+    private static Cell InsertCell(Worksheet worksheet, Row row, string columnName, uint rowIndex) {
+      
+      string cellReference = columnName + rowIndex;
+
+      if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0) {
+        return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+      } else {       
+        Cell refCell = null;
+        foreach (Cell cell in row.Elements<Cell>()) {
+          if (string.Compare(cell.CellReference.Value, cellReference, true) > 0) {
+            refCell = cell;
+            break;
+          }
+        }
+
+        Cell newCell = new Cell() { CellReference = cellReference };
+        row.InsertBefore(newCell, refCell);
+
+        worksheet.Save();
+        return newCell;
+      }
+
+    }
+
+
 
   }
 }
